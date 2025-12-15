dfdf8ab660221662c475b05ae18749118f42825d
tanyabansal1404 <tanyabansal1404@gmail.com>
2025-12-15 22:11:59 +0530

    Dynamic Prompt

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,6 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 
@@ -8,10 +9,30 @@ model = ChatOpenAI(model='gpt-4')
 
 st.header('Research Assistant')
 
-user_input = st.text_input("Enter your prompt")
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = load_prompt('template.json')
+
+# fill the placeholders
+# prompt = template.invoke({
+#     'paper_input': paper_input,
+#     'style_input': style_input,
+#     'length_input': length_input
+# })
 
 if st.button('Summarize'):
-    result = model.invoke(user_input)
+    chain = template | model
+    result = chain.invoke({
+        'paper_input': paper_input,
+        'style_input': style_input,
+        'length_input': length_input
+    })
+
+    # result = model.invoke(prompt)
     st.write(result.content)
 
 
