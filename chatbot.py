bbd1593a07a063ba3a9fb8d74bfa680bb7fa33ef
tanyabansal1404 <tanyabansal1404@gmail.com>
2025-12-15 22:33:42 +0530

    Chatbot Within Minutes

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,22 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 load_dotenv()
 
+model = ChatOpenAI()
+
+chat_history = [
+    SystemMessage(content="You are a helpful AI assistant")
+]
+
+while True:
+    user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
+    if user_input == 'exit':
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.content))
+    print('AI: ', result.content)
+
+print(chat_history)
