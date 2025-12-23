88b3146cb221bc27b686e6bfb3fa77b5d639c421
tanyabansal1404 <tanyabansal1404@gmail.com>
2025-12-23 19:57:03 +0530

    Final Commit

--- prompt_ui.py
+++ prompt_ui.py
@@ -9,11 +9,31 @@ model = ChatOpenAI(model='gpt-4')
 
 st.header('Research Assistant')
 
-paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+# user_input = st.text_input('Enter your prompt')
 
-style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+paper_input = st.selectbox("Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
-length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+style_input = st.selectbox("Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox("Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+# template
+# template = PromptTemplate(
+#     template="""
+#     Please summarize the research paper titled "{paper_input}" with the following specifications:
+#     Explanation Style: {style_input}  
+#     Explanation Length: {length_input}  
+#     1. Mathematical Details:  
+#      - Include relevant mathematical equations if present in the paper.  
+#      - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
+#     2. Analogies:  
+#      - Use relatable analogies to simplify complex ideas.  
+#     If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
+#     Ensure the summary is clear, accurate, and aligned with the provided style and length.
+# """,
+# input_variables=['paper_input', 'style_input', 'length_input'],
+# validate_template=True
+# )
 
 template = load_prompt('template.json')
 
@@ -32,6 +52,7 @@ if st.button('Summarize'):
         'length_input': length_input
     })
 
+    # result = model.invoke(user_input)
     # result = model.invoke(prompt)
     st.write(result.content)
 
